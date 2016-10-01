59be0bb2761096dd788cb58cfba2ca8020189d72
svasenkin <svasenkin@live.com>
2016-10-01 18:07:43 +0300

    build 01 10 16

--- pdox2xml/Program.cs
+++ pdox2xml/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using ParadoxReader;
 using System.Windows.Forms;
 using System.IO;
+using System.Xml.Linq;
 
 namespace pdox2xml
 {
@@ -21,20 +22,32 @@ namespace pdox2xml
             if(dlg.ShowDialog() == DialogResult.OK)
             {
                 ParadoxTable tbl = new ParadoxTable(dlg.FileName, "tbl");
-              /*  int recIndex = 0;
+                XElement root = new XElement("Data");
+                string[] fnames = tbl.FieldNames;
+
                 foreach(var rec in tbl.Enumerate())
                 {
-                    Console.WriteLine("Record #{0}", recIndex++);
-                    for (int i = 0; i < tbl.FieldCount; i++)
-                    {
-                        Console.WriteLine("    {0} = {1}", tbl.FieldNames[i], rec.DataValues[i]);
-                    }
-                    if (recIndex > 10) break;
+                    XElement trec = new XElement("Record", from c in rec.DataValues.AsEnumerable()
+                                                           select new XElement("", c));
+                    root.Add(trec);
                 }
-                */
-                //tbl.
+                
+                /*  int recIndex = 0;
+                  foreach(var rec in tbl.Enumerate())
+                  {
+                      Console.WriteLine("Record #{0}", recIndex++);
+                      for (int i = 0; i < tbl.FieldCount; i++)
+                      {
+                          Console.WriteLine("    {0} = {1}", tbl.FieldNames[i], rec.DataValues[i]);
+                      }
+                      if (recIndex > 10) break;
+                  }
+                  */
 
+              /*  XElement data = new XElement("data", from c in tbl.Enumerate()
+                                                     select new XElement("", c.DataValues));*/
                 string newpath = Path.ChangeExtension(dlg.FileName, ".xml");
+                root.Save(newpath);
                 Console.WriteLine(newpath);
 
                 Console.ReadKey();
